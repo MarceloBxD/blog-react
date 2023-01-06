26fe6690b6d7c76dd3c04f1c66f7806d9c26c2f3
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-06 02:27:09 -0300

    adding pics and chaning styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import image1 from "../assets/images/image1.jpg";
+import image2 from "../assets/images/image2.jpg";
+import image3 from "../assets/images/image3.jpg";
+import image4 from "../assets/images/image4.jpg";
 import "../style.scss";
 
 export const Home = () => {
   const posts = [
     {
       id: 1,
-      title: "Post 1",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-      photo:
-        "https://images.unsplash.com/photo-1610395697869-8b5f1b2b1b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq caractur abla dimasiado inspect mint flote ipsum dolor sit amet consecterur Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq.",
+      photo: `${image1}`,
     },
     {
       id: 2,
-      title: "Post 2",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-      photo:
-        "https://images.unsplash.com/photo-1610395697869-8b5f1b2b1b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq",
+      photo: `${image2}`,
     },
     {
       id: 3,
-      title: "Post 3",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-      photo:
-        "https://images.unsplash.com/photo-1610395697869-8b5f1b2b1b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowqLorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq",
+      photo: `${image3}`,
     },
     {
       id: 4,
-      title: "Post 4",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-      photo:
-        "https://images.unsplash.com/photo-1610395697869-8b5f1b2b1b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quodowq",
+      photo: `${image4}`,
     },
   ];
 
@@ -41,8 +45,8 @@ export const Home = () => {
           <div key={post.id} className="post">
             <img src={post.photo} alt="photo can't load" className="img" />
             <div className="post-info">
-              <Link to={`/posts/${post.id}`}>
-                <h1 className="title">{post.title}</h1>
+              <Link className="link" to={`/posts/${post.id}`}>
+                <h1>{post.title}</h1>
               </Link>
               <div className="post-desc">{post.desc}</div>
               <button>read more</button>
