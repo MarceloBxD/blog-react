35b11d3d7c3e3713340b6f761e407156a34e7115
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-06 02:37:24 -0300

    adding logo img

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import logo from "../assets/images/logo.jpg";
 import "../style.scss";
 
 export const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="logo">
+        <img className="logo-img" src={logo} alt="logo" />
+      </div>
+      <div className="message">Made with love by Marcelo Bracet</div>
+    </div>
+  );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.jpg";
 import "../style.scss";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo"></div>
+      <div className="logo">
+        <img src={logo} className="logo-img" />
+      </div>
       <div className="nav-items">
         <div className="nav-item">Art</div>
         <div className="nav-item">Science</div>
